85ee90e11b734a00683c8cc8e9e747e4f80d04d4
David Fallah <davidfallah1@gmail.com>
2017-10-01 01:07:35 +0100

    Add explanation for game setup

--- Program.cs
+++ Program.cs
@@ -16,6 +16,15 @@ namespace experiment
             }
         }
 
+        /// <remarks>
+        /// This is hardcoded here just as an example.
+        /// <para></para>
+        /// A better way to do it would be to define all scenarios and choices as JSON and deserialise
+        /// them during application startup. That way you cleanly separate code and data, and don't need
+        /// to modify existing code and recompile just to expand the game.
+        /// <para></para>
+        /// I've implemented JSON deserialisation support but haven't tested it out.
+        /// </remarks>
         static GameSetup CreateGameSetup()
         {
             const string StartVillage = "START_VILLAGE";
